50af6f70ea77512361ab5931ef95bd3dfb0b1d9a
GOMIERO Matthieu <matthieu.gomiero@akkodis.com>
2026-06-07 11:35:50 +0200

    Bazard

--- Assets/StarBehavior.cs
+++ Assets/StarBehavior.cs
@@ -4,15 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class StarBehavior : MonoBehaviour
 {
-    public float distanceFromPlayer = 50;
+    private float distanceFromPlayer;
     public float minimalDistance= 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        distanceFromPlayer = Random.value * 100.0f;
 
-        this.transform.position = new Vector3(Random.value, Random.value, Random.value) * distanceFromPlayer + new Vector3(minimalDistance, minimalDistance, minimalDistance);
+		this.transform.position = Random.onUnitSphere * distanceFromPlayer + new Vector3(minimalDistance, minimalDistance, minimalDistance);
         this.transform.LookAt(Camera.main.transform);
     }
+
+
+
 
     // Update is called once per frame
     void Update()
