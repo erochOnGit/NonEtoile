f9ce705a229b85596947ee8bbeaa977a0a0e4d4b
GOMIERO Matthieu <matthieu.gomiero@akkodis.com>
2026-06-07 13:00:53 +0200

    PostProcess

--- Assets/RotateTowardsCam.cs
+++ Assets/RotateTowardsCam.cs
@@ -5,12 +5,12 @@ public class RotateTowardsCam : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+		transform.LookAt(transform.position + Camera.main.transform.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-		transform.LookAt(transform.position + Camera.main.transform.forward);
 	}
 }
